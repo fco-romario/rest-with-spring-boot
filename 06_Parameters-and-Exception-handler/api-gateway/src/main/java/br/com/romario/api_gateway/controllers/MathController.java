4fe7684cb2225cc71e38577afd182ca7889c8a9e
fco-romario <fco.romario.alves@gmail.com>
2024-12-28 19:58:41 -0300

    Tratamento de Exceções

--- 06_Parameters-and-Exception-handler/api-gateway/src/main/java/br/com/romario/api_gateway/controllers/MathController.java
+++ 06_Parameters-and-Exception-handler/api-gateway/src/main/java/br/com/romario/api_gateway/controllers/MathController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.romario.api_gateway.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 
@@ -20,7 +22,7 @@ public class MathController {
     public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
     	
     	if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-    		throw new Exception();
+    		throw new UnsupportedMathOperationException("Please set a numeric value");
     	}
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
