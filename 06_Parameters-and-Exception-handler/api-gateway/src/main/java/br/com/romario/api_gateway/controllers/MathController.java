ce8547b784f9291bd8cb9ff112fe2f3b5ecc2b83
fco-romario <fco.romario.alves@gmail.com>
2024-12-27 22:20:27 -0300

    @PathVariable e @RequestParam. Path parameters e Query parameters

--- 06_Parameters-and-Exception-handler/api-gateway/src/main/java/br/com/romario/api_gateway/controllers/MathController.java
+++ 06_Parameters-and-Exception-handler/api-gateway/src/main/java/br/com/romario/api_gateway/controllers/MathController.java
@@ -2,20 +2,40 @@ package br.com.romario.api_gateway.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.romario.api_gateway.models.Greeting;
-
 @RestController
-public class GreetingController {
+public class MathController {
 
     private static final String template = "Hello, %s!";
     private static final AtomicLong count = new AtomicLong();
-
-    @RequestMapping("/api/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(count.incrementAndGet(), String.format(template, name));
+    
+    //@PathVariable : Path Parameters é usado quando o parametro é obrigatório. é necesário inserir o "value" -> http://localhost:8080/api/sum/1/2
+    //@RequestParam : Query Parameters é usado quando o parametro não são obrigatório -> localhost:8080/api/greeting?name=romario
+    
+    @RequestMapping(value = "/api/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET) 
+    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    	
+    	if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+    		throw new Exception();
+    	}
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
+	
+	private Double convertToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		// BR 10,25 US 10.25
+		String number = strNumber.replaceAll(",", ".");
+		if(isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+	
+	private boolean isNumeric(String strNumber) {		
+		if(strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
 }
