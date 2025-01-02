729695f2341e4ba9248b4a51dec47f8a1892dbc8
fco-romario <fco.romario.alves@gmail.com>
2025-01-01 22:23:28 -0300

    Refatorando as anotações @RequestMapping para @GetMapping etc.

--- 08_Integracao-API-ao-banco-MySQL/api-gateway/src/main/java/br/com/romario/api_gateway/controllers/PersonController.java
+++ 08_Integracao-API-ao-banco-MySQL/api-gateway/src/main/java/br/com/romario/api_gateway/controllers/PersonController.java
@@ -5,7 +5,11 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,35 +30,32 @@ public class PersonController {
     //@PathVariable : Path Parameters é usado quando o parametro é obrigatório. é necesário inserir o "value" -> http://localhost:8080/api/sum/1/2
     //@RequestParam : Query Parameters é usado quando o parametro não são obrigatório -> localhost:8080/api/greeting?name=romario
     
-    @RequestMapping(method= RequestMethod.GET,
-    		produces = MediaType.APPLICATION_JSON_VALUE) 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) 
     public List<Person> findAll() {
     	return personService.findAll();
     }
     
-    @RequestMapping(value = "/{id}",
-    		method= RequestMethod.GET,
+    @GetMapping(value = "/{id}",
     		produces = MediaType.APPLICATION_JSON_VALUE) 
     public Person findById(@PathVariable(value = "id") Long id) {
     	return personService.findById(id);
     }
     
-    @RequestMapping(method= RequestMethod.POST,
+    @PostMapping(
     		produces = MediaType.APPLICATION_JSON_VALUE,
     		consumes = MediaType.APPLICATION_JSON_VALUE) 
     public Person create(@RequestBody Person person) {
     	return personService.create(person);
     }
     
-    @RequestMapping(method= RequestMethod.PUT,
+    @PutMapping(
     		produces = MediaType.APPLICATION_JSON_VALUE,
     		consumes = MediaType.APPLICATION_JSON_VALUE) 
     public Person Update(@RequestBody Person person) {
     	return personService.update(person);
     }
     
-    @RequestMapping(value = "{id}",
-    		method = RequestMethod.DELETE)
+    @DeleteMapping(value = "{id}")
     public void delete(@PathVariable(value = "id") Long id) {
     	personService.delete(id);
     }
