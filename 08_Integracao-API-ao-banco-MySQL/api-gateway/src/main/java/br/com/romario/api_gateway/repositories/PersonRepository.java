2562f82212f3b28cabc9d54627c10e41689f8970
fco-romario <fco.romario.alves@gmail.com>
2025-01-01 21:31:41 -0300

    Refatorando UnsupportedMathOperationException para ResourceNotFoundException

--- 08_Integracao-API-ao-banco-MySQL/api-gateway/src/main/java/br/com/romario/api_gateway/repositories/PersonRepository.java
+++ 08_Integracao-API-ao-banco-MySQL/api-gateway/src/main/java/br/com/romario/api_gateway/repositories/PersonRepository.java
@@ -1,5 +1,11 @@
 package br.com.romario.api_gateway.repositories;
 
-public interface PersonRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.romario.api_gateway.models.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
 
 }
