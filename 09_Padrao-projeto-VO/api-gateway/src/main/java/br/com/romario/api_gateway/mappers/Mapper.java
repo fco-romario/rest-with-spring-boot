471a452175e26816f0a55fbfb943934a9c43ea1f
fco-romario <fco.romario.alves@gmail.com>
2025-01-04 19:06:38 -0300

    Usando ModelMapper ao invés do DozerMapper

--- 09_Padrao-projeto-VO/api-gateway/src/main/java/br/com/romario/api_gateway/mappers/Mapper.java
+++ 09_Padrao-projeto-VO/api-gateway/src/main/java/br/com/romario/api_gateway/mappers/Mapper.java
@@ -1,15 +1,17 @@
-package br.com.romario.api_gateway.mapper;
+package br.com.romario.api_gateway.mappers;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+import org.modelmapper.ModelMapper;
 
-public class DozerMapper {
+//import com.github.dozermapper.core.DozerBeanMapperBuilder;
+//import com.github.dozermapper.core.Mapper;
+
+public class Mapper {
 	
-	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	//private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	private static ModelMapper mapper = new ModelMapper();
 	
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
--- 09_Padrao-projeto-VO/api-gateway/src/main/java/br/com/romario/api_gateway/services/PersonService.java
+++ 09_Padrao-projeto-VO/api-gateway/src/main/java/br/com/romario/api_gateway/services/PersonService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.romario.api_gateway.data.vo.v1.PersonVO;
 import br.com.romario.api_gateway.exceptions.ResourceNotFoundException;
-import br.com.romario.api_gateway.mapper.DozerMapper;
+import br.com.romario.api_gateway.mappers.Mapper;
 import br.com.romario.api_gateway.models.Person;
 import br.com.romario.api_gateway.repositories.PersonRepository;
 
@@ -23,7 +23,7 @@ public class PersonService {
 	public List<PersonVO> findAll() {
 		logger.info("Finding all people");	
 		
-		List<PersonVO> people = DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+		List<PersonVO> people = Mapper.parseListObjects(personRepository.findAll(), PersonVO.class);
 		
 		if(people.isEmpty()) throw new ResourceNotFoundException("No records found");
 		return people;
@@ -34,35 +34,35 @@ public class PersonService {
 		
 		var entity =  personRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID " + id));
-		return DozerMapper.parseObject(entity, PersonVO.class);
+		return Mapper.parseObject(entity, PersonVO.class);
 	}
 	
 	public PersonVO create(PersonVO person) {
 		logger.info("Creating one person");
 		
-		var entity = DozerMapper.parseObject(person, Person.class);
-		var vo = DozerMapper.parseObject(personRepository.save(entity), PersonVO.class);	
+		var entity = Mapper.parseObject(person, Person.class);
+		var vo = Mapper.parseObject(personRepository.save(entity), PersonVO.class);	
 		return vo;
 	}
 	
 	public PersonVO update(PersonVO person) {
 		logger.info("Updating one person");
 		
-		var entity = DozerMapper.parseObject(findById(person.getId()),  Person.class);
+		var entity = Mapper.parseObject(findById(person.getId()),  Person.class);
 		
 		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
 		
-		var vo = DozerMapper.parseObject(personRepository.save(entity), PersonVO.class);
+		var vo = Mapper.parseObject(personRepository.save(entity), PersonVO.class);
 		return vo;
 	}
 	
 	public void delete(Long id) {
 		logger.info("Deleting one person");
 		
-		var entity = DozerMapper.parseObject(findById(id),  Person.class);
+		var entity = Mapper.parseObject(findById(id),  Person.class);
 		personRepository.delete(entity);
 	}
 }
